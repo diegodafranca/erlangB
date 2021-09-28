0e3e3b9e1dc846b1777354e09e2f3483865ad8f5
Diego Medeiros <71795546+diegodafranca@users.noreply.github.com>
2021-09-28 16:35:44 -0300

    Update ErlangB Calculator.py
    Adding comments to the functions.

--- ErlangB Calculator.py
+++ ErlangB Calculator.py
@@ -2,6 +2,7 @@
 from math import ceil
 
 def probability(p,c):
+  #this function returns the probability of not answering a call given the BHC Volume in Erlangs and c the number of phone lines 
   if(p == 0):
     return 0.0
   s= 0.0
@@ -11,6 +12,7 @@ def probability(p,c):
   return pc
 
 def lines(p,prob):
+  #this function returns the number of lines given certain values of p and prob, where p is the BHC Volume in Erlangs and prob the probability of not answering a call 
   l,r = 0,ceil(p)
   fR = probability(p,r)
   while(fR > prob):
@@ -26,13 +28,10 @@ def lines(p,prob):
       r = mid
   return r - 1
 
-
-while(True):
-  Ch = int(input("Insert BHC:"))
-  AvgCallDuration = float(input("Insert call duration in seconds: "))
-  AvgCallDuration = AvgCallDuration/3600
-  p = Ch*AvgCallDuration
-  prob = 0.01
-  #prob is the probability of a call being rejected given the BHC and average call duration. It needs to be > 0 and <=1.
-  print("For the volume of",p,"erlangs there will be a need for",lines(p,prob), "lines in order to attend the grade of service of",int(prob*100), "%.")
-  break
+Ch = int(input("Insert BHC:"))
+AvgCallDuration = float(input("Insert call duration in seconds: "))
+AvgCallDuration = AvgCallDuration/3600 #convert from seconds to hour
+p = Ch*AvgCallDuration
+prob = 0.01
+#prob is the probability of a call being rejected given the BHC and average call duration. It needs to be > 0 and <=1.
+print("For the volume of",p,"erlangs there will be a need for",lines(p,prob), "lines in order to attend the grade of service of",int(prob*100), "% blocking probability.")
